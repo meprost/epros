6cd16e608dece15aa2c1a0af4b3080c04a2f961a
meprost <cherylrio.school@gmail.com>
2023-06-21 23:10:24 +0700

    commit pertama

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className='flex justify-center items-center min-h-[100vh]'>
+      <h1 className='text-9xl'>Halo, meprost</h1>
+    </main>
+  )
+}
