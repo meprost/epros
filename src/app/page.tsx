c921ad2a4bfce707be1626c2624a06a5d9c9f043
meprost <cherylrio.school@gmail.com>
2023-06-21 23:13:30 +0700

    menambahkan tulisan

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className='flex justify-center items-center min-h-[100vh]'>
-      <h1 className='text-9xl'>Halo, meprost</h1>
+      <h1 className='text-9xl'>Halo, meprost dari github</h1>
     </main>
   )
 }
