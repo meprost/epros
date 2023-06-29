a565fe11d54f2a78291e5cb0508837694fb305f9
meprost <cherylrio.school@gmail.com>
2023-06-29 21:45:46 +0700

    FixBug2

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,8 @@
 export default function Footer() {
   return (
-    <div className="absolute bottom-0 text-center w-full py-2 text-sm text-white">
-      Copyright &#169; 2023 by Meprost.corps - All Rights Reserved
+    <div className="absolute bottom-0 gap-x-2 md:flex-row justify-center text-center flex flex-col w-full py-2 text-sm text-white">
+        <p>Copyright &#169; 2023 by Meprost.corps {' '}</p>
+       <p>- All Rights Reserved</p>
     </div>
   );
 }
